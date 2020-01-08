b12b6ff9d340ab7e01506bd1f5230f1212260ad2
dontdrinkmylemmontea <492861128@qq.com>
2020-01-08 15:41:25 +0800

    修改商品hover样式，删去背景图

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,67 @@
+import React, { Component } from 'react';
 import styles from './index.css';
 import hamburger from '../assets/img/hamburger.svg';
-import algo from '../assets/img/algo.svg';
-import tree from '../assets/img/tree.svg';
-import css from '../assets/img/css.svg';
-import clock from '../assets/img/clock.svg';
 
 const goods = [
   {
     title: '二分查找',
     description: '比较二分搜索和顺序搜索在前端执行的效率',
-    image: algo,
   },
   {
     title: '吸猫处',
     description: 'flex瀑布流，随机猫猫图片',
-    image: css,
   },
   {
     title: '四叉树',
     description: '四叉树在web前端的应用',
-    image: tree,
   },
   {
     title: '字典树',
     description: '字典树在web前端的应用',
-    image: tree,
   },
   {
     title: '更多作品敬请期待',
     description: "(●'◡'●)",
-    image: clock,
   },
 ];
 
-export default function() {
-  return (
-    <div>
-      <div className={styles.banner}>
-        <a href="/">
-          <img src={hamburger} alt="icon" className={styles.bannerIcon} />
-        </a>
-        <div className={styles.bannerText}>Hamburger</div>
+const generateStyles = args =>
+  Object.keys(args)
+    .filter(key => args[key])
+    .map(key => styles[key])
+    .join(' ');
+
+class pageName extends Component {
+  state = {
+    currentGood: 0,
+  };
+
+  onChangeGood = goodIndex => this.setState({ currentGood: goodIndex });
+
+  render() {
+    const { currentGood } = this.state;
+    return (
+      <div className={styles.root}>
+        <div className={styles.banner}>
+          <a href="/">
+            <img src={hamburger} alt="icon" className={styles.bannerIcon} />
+          </a>
+          <div className={styles.bannerText}>Hamburger</div>
+        </div>
+        <div className={styles.goodsBar}>
+          {goods.map(({ title, description }, key) => (
+            <div
+              key={key}
+              className={generateStyles({ good: true, active: currentGood === key })}
+              onClick={() => this.onChangeGood(key)}
+            >
+              <div className={styles.title}>{title}</div>
+              <div className={styles.desc}>{description}</div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className={styles.goodsBar}>
-        {goods.map(({ image, title, description }, key) => (
-          // <div key={key} className={styles.good}>
-          <div key={key} className={styles.good} style={{ backgroundImage: `url(${image})` }}>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.desc}>{description}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
+export default pageName;
